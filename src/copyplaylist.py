830ff7e196ce290583f4eae67efb2bd0e6291a29
Paul-Vincent Roll <paviro@me.com>
2023-02-26 00:05:48 +0100

    Improved user interaction flow

--- src/copyplaylist.py
+++ src/copyplaylist.py
@@ -3,8 +3,19 @@ from plexapi.exceptions import NotFound
 
 from getpass import getpass
 
+import os
+
+import time
+
 import re
 
+
+#
+# Clear the screen 
+# uses 'cls' on windows and 'clear' on linux / macos
+#
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
 
 #
 # Given a list, select a single item from the list, or return None if they skip the selection
@@ -57,42 +68,52 @@ def simplify_string(source_str):
 # source_item - The item that we should be finding a match for on the target server
 #
 def find_matching_item(source_item, target_server):
-    print("     Searching for tracks in target server")
+    print("\nSearching for tracks on target server...")
     matching_tracks = target_server.search(source_item.title, mediatype='track', limit=100)
     matched_track = None
     for track in matching_tracks:
         if simplify_string(track.title) == simplify_string(source_item.title) and simplify_string(track.parentTitle) \
                 == simplify_string(source_item.parentTitle):
-            print(
-                f"     Found exact match for Title: {track.title}, Album: {track.parentTitle}, Artist: {track.grandparentTitle}")
+            print("\nFound exact match!")
+            time.sleep(1)
             matched_track = track
             break
     if matched_track is None:
         print("")
-        print(
-            f"     Did not find an exact match, but these are close matches to Title: {source_item.title}, Album: {source_item.parentTitle}, Artist: {source_item.grandparentTitle}")
+        
         matching_tracks = target_server.search(simplify_string(source_item.title), mediatype='track', limit=100)
-        selection = select_item(matching_tracks, "     Select matching track number or 'n' for None >>>",
-                                "     %(index)x: Title: %(title)s, Album: %(parentTitle)s, Artist: %(grandparentTitle)s"
-                                )
-
+        if len(matching_tracks) > 0:
+            print("No exact match found, but these are very similar:\n")
+            selection = select_item(matching_tracks, "\nSelect matching track number or 'n' to skip the track: ",
+                                "     %(index)x: Title: %(title)s, Album: %(parentTitle)s, Artist: %(grandparentTitle)s")
+        else:
+            print("No exact match or similar track found.\n")
+            input("(Press enter to continue and skip track)")
+            selection = None
+            
         if selection is not None:
             matched_track = matching_tracks[int(selection)]
             print(f"     Adding track {matched_track.title}, {matched_track.parentTitle}")
-        else:
-            print("     Skipping matching this track....")
-            print("")
+            
 
     return matched_track
 
 
 #
-# Login to your plex account, and list out all possible devices by name.  Servers and clients will show up here, so
+# Login to your plex account, and list out all possible devices by name.  Servers will show up here, so
 # just allow the user to choose which one they want.  Then choose the playlist from the playlists on the source server
 # and ask for the user to provide a name for the playlist on the target server.  If the playlist exists, perform
 # a merge of the data.  If not, create a new playlist with all matched items.
 #
 def main():
+    
+    cls()
+    print("##############################")
+    print("#     Plex Playlist Copy     #")
+    print("##############################")
+    print("To start, please log in to your Plex account.")
+    print("")
+    
     user = input("Enter username: ")
     password = getpass("Enter Password: ")
     
@@ -120,37 +141,79 @@ def main():
         if resource.product == "Plex Media Server":
             available_servers.append(resource)
         
+    cls()
+    print("################################")
+    print("#     Select source server     #")
+    print("################################")
     print("")
-    selection = select_item(available_servers, "Select the server which you wish to copy a playlist from >>>",
+    
+    selection = select_item(available_servers, "\nSelect the server from which you want to copy a playlist: ",
                             "%(index)x: %(name)s")
     source_server = available_servers[selection].connect()
-    source_playlists = source_server.playlists()
+    
+    # Remove source server from the list so that it is not available as a destination
+    available_servers.pop(selection)
+    
+    cls()
+    print("#####################################")
+    print("#     Select destination server     #")
+    print("#####################################")
     print("")
-    selection = select_item(source_playlists, "Select the number of the playlist to copy from >>>",
+    
+    selection = select_item(available_servers, "\nSelect the server to which you want to copy the playlist: ",
+                            "%(index)x: %(name)s")
+    target_server = available_servers[selection].connect()
+    
+    cls()
+    print("###########################")
+    print("#     Select playlist     #")
+    print("###########################")
+    print("")
+    
+    source_playlists = source_server.playlists()
+    
+    selection = select_item(source_playlists, "\nSelect the number of the playlist you want to copy: ",
                             "%(index)x: %(title)s")
     source_playlist = source_playlists[selection]
 
-    selection = select_item(available_servers, "Please select the server you wish to copy the playlist to >>>",
-                            "%(index)x: %(name)s")
-    target_server = available_servers[selection].connect()
-
+    cls()
+    print("##########################")
+    print("#     Name selection     #")
+    print("##########################")
     print("")
-    target_playlist_title = input("Please enter the name to use for the target playlist >>>")
+    
+    while True:
+        use_original_name = input("Do you want to keep the same name for the playlist on the destination server? (y/n): ").lower().strip()
+        if use_original_name == "y":
+            target_playlist_title = source_playlist.title
+            break
+        elif use_original_name == "n":
+            target_playlist_title = input("\nEnter the name to be used for the copied playlist: ")
+            break
+        else:
+            continue
+    
     try:
         target_playlist = target_server.playlist(target_playlist_title)
-        print("Target playlist exists, so updating any missing tracks")
+        print("Target playlist already exists, all missing tracks will be added.")
     except NotFound:
-        print("Target playlist does not exist, will create a new playlist")
+        print("Target playlist does not exist, a new playlist will be created.")
         target_playlist = None
 
     total_tracks = 0
     unmatched_items = []
     playlist_items = []
     for item in source_playlist.items():
+        
+        cls()
+        print("##########################")
+        print("#     Name selection     #")
+        print("##########################")
         print("")
+        
         total_tracks += 1
-        print(
-            f"Attempting to match track # {total_tracks}, Title:{item.title}, Album: {item.parentTitle}, Artist: {item.grandparentTitle}")
+        print(f"Attempting to match track #{total_tracks}:\n\nTitle:{item.title}\nAlbum: {item.parentTitle}\nArtist: {item.grandparentTitle}\n")
+        print("-----------------------------")
 
         matched_track = None
         try:
@@ -181,3 +244,4 @@ def main():
 #
 if __name__ == '__main__':
     main()
+    
