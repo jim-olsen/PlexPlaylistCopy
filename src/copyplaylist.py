b2b5c1346dcf2c44e670b03d93cd4c27fab0878a
Paul-Vincent Roll <paviro@me.com>
2023-02-26 13:47:13 +0100

    Removed stray print statement

--- src/copyplaylist.py
+++ src/copyplaylist.py
@@ -89,7 +89,7 @@ def find_matching_item(source_item, target_server, section=None):
             
         if simplify_string(track.title) == simplify_string(source_item.title) and simplify_string(track.parentTitle) \
                 == simplify_string(source_item.parentTitle) and distance(track.duration, source_item.duration) < 1000:
-            print(distance(track.duration, source_item.duration))
+
             print("\nFound exact match!")
             time.sleep(0.3)
             matched_track = track
